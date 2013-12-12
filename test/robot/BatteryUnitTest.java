3460c12283071db91b904c5fef3233bca9c04d13
jrigault <jrigault@live.fr>
2013-12-12 15:00:10 +0100

    a

--- test/robot/BatteryUnitTest.java
+++ test/robot/BatteryUnitTest.java
@@ -4,20 +4,38 @@ package robot;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class BatteryUnitTest {
 
+
+    // méthode chargeFunction en privée
 
     @Test
     public void testCharge(){
         Battery battery = new Battery();
         battery.charge();
+        Assert.assertEquals(110,battery.getChargeLevel(),0);
     }
 
+    @Test
+    public void testSetUp(){
+        // a faire
+    }
 
+    @Test
+    public void testGetChargeLevel(){
+        Battery battery = new Battery();
+        Assert.assertEquals(100,battery.getChargeLevel(),0);
+    }
 
-
-
+    @Test
+    public void testTimeToSufficientCharge(){
+        Battery battery=new Battery();
+        Assert.assertEquals(1000,battery.timeToSufficientCharge(110),0);
+    }
 
 
 
